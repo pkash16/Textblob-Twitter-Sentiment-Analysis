c3b3e9576757369b07bbf605dd6d69fe3d1a0c1a
pkash16 <p.kash16@gmail.com>
2015-07-14 15:14:22 -0400

    finally got json to parse properly

--- test.py
+++ test.py
@@ -2,29 +2,27 @@ from textblob import TextBlob
 import tweetpony
 import json
 import jsonpickle
+from unidecode import unidecode
 
-
-text = raw_input("Give me text to analyze sentiment\n")
-
-wiki = TextBlob(text);
-
-print(wiki.sentiment)
-
-
+total_sentiment = 0
 
 api = tweetpony.API(consumer_key = "5XTZQYKqXirfeKprCQIlLGv0K", consumer_secret = "sgInLsaDTKYmsrNsiBfW7VqDXOv56bHssndhtWrwdx2U9nlOes", access_token = "1737850004-sl6kr0L2yrUDFmqgJvzSlJmfYBtjK4XCcYLDAqY", access_token_secret = "ETZN5I6B7n9Y2Svy6qV0ZkAkboN48AyqPFFmVBrfKu3YZ")
 user = api.user
 
 input_query = raw_input("Type in an a query\n")
 
-json_data = api.search_tweets(q=input_query, lang='en', count=1)
+json_data = api.search_tweets(q=input_query, lang='en', count=200)
 data = json.loads(jsonpickle.encode(json_data))
 
-print(data["statuses"])
+newdata = json.loads(data["json"])
 
 
-
-
+for x in range(0,len(newdata['statuses'])):
+	print(unidecode(newdata['statuses'][x]['text']))
+	sent = TextBlob(unidecode(newdata['statuses'][x]['text']))
+	total_sentiment += (sent.sentiment.polarity + 1)
+	
+	print(sent.sentiment.polarity);
 
 '''
 
